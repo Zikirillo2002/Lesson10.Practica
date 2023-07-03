1ce892d20775f15207d55f7433f459f3d8143fc9
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2023-07-03 15:40:06 +0500

    Creat new project

---
+++ Lesson10.Practica/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson10.Practica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
